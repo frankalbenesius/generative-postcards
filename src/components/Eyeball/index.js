8c3e01866807fa5b2dd77fb60211546c3211b350
Frank Albenesius <frankalbenesius@gmail.com>
2018-12-27 17:41:48 -0600

    add storybook

---
+++ src/components/Eyeball/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default () => {
+  return (
+    <svg width="100" height="100">
+      <circle
+        cx="50"
+        cy="50"
+        r="25"
+        stroke="black"
+        strokeWidth="3"
+        fill="none"
+      />
+    </svg>
+  );
+};
---
+++ src/components/Eyeball/stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Eyeball from "./";
+
+storiesOf("Eyeball", module).add("default", () => <Eyeball />);
